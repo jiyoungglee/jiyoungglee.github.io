91d7199ebc28d27a39eee874bfd27e617e76a9f8
Jiyoung Lee <jiyoung.g.lee118@gmail.com>
2022-09-08 12:48:24 -0400

    CSS styling for selected project items and play button scope

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faAngleRight, faLink, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleRight, faLink, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 
 import '../Styles/ProjectItem.css';
 import { useContext } from "react";
@@ -8,7 +8,7 @@ import { Actions, PlayerContext } from "../store/PlayerContext";
 function ProjectItem({ index, link, name, description, demo, preview, descOpen, setDescOpen }) {
   const { state, dispatch } = useContext(PlayerContext);
 
-  function select() {
+  function seeDescription() {
     if (descOpen === index) {
       setDescOpen(null)
     } else {
@@ -30,20 +30,24 @@ function ProjectItem({ index, link, name, description, demo, preview, descOpen, 
   }
 
   return (
-    <div className="project">
-      <div className={`project-item${descOpen===index ? " selected" : ""}`} onClick={select}>
+    <div className={`project${descOpen===index ? " selected" : ""}`}>
+      <div className="project-item">
         <span className="project-number">{index}</span>
         <div className="project-icon" onClick={play}>
-          <FontAwesomeIcon icon={faPlay} />
+          {state.index===index ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} /> }
         </div>
         <div className="project-icon">
           <a href={link} target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLink} />
           </a>
         </div>
-        <span className="project-name">{name}</span>
-        {descOpen===index ? <FontAwesomeIcon icon={faAngleDown} /> : <FontAwesomeIcon icon={faAngleRight} />}
-        {state.index===index && <div>Playing...</div>}
+        <div className="project-text" onClick={play}>
+          <div className="project-name">{name}</div>
+          {state.index===index && <div>Playing...</div>}
+        </div>
+        <div onClick={seeDescription} className="project-icon">
+          {descOpen===index ? <FontAwesomeIcon icon={faAngleDown} /> : <FontAwesomeIcon icon={faAngleRight} />}
+        </div>
       </div>
       {descOpen===index 
         && 
